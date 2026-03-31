a03e49ea0d56e00c5b870d5fe8df4da064acf6d5
Don Maslanka <don.maslanka@toptal.com>
2026-03-30 21:40:35 -0600

    Fix: update testCaseId paths in CreditCard.ts (Card->CreditCard) and CreditLine.ts (Creditline->CreditLine)

--- Test Suites/CreditCard.ts
+++ Test Suites/CreditCard.ts
@@ -15,56 +15,56 @@
       <guid>c8f7845b-f9eb-4405-a3de-5bd6c1fd726e</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Card/card01</testCaseId>
+      <testCaseId>Test Cases/CreditCard/card01</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>37b3eca8-8701-4f68-b7f1-7cec250b6998</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Card/card02</testCaseId>
+      <testCaseId>Test Cases/CreditCard/card02</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>045bb9f1-3c9b-4a92-8954-e4a32807e7d4</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Card/card03</testCaseId>
+      <testCaseId>Test Cases/CreditCard/card03</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>d5332f3c-e83a-4c6b-af1a-4ed175ec52c1</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Card/card04</testCaseId>
+      <testCaseId>Test Cases/CreditCard/card04</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>3af01b6c-70cb-4a04-ba20-8515a75f7539</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Card/card05</testCaseId>
+      <testCaseId>Test Cases/CreditCard/card05</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>1796eced-946c-46ea-88f9-b226c823003d</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Card/card06</testCaseId>
+      <testCaseId>Test Cases/CreditCard/card06</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>3a792316-8b7d-4e39-812e-6dd779eaadfe</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Card/card07</testCaseId>
+      <testCaseId>Test Cases/CreditCard/card07</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>0848c68a-a5f9-4aae-bc93-34fecf5a1d77</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Card/card08</testCaseId>
+      <testCaseId>Test Cases/CreditCard/card08</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
--- Test Suites/CreditLine.ts
+++ Test Suites/CreditLine.ts
@@ -15,49 +15,49 @@
       <guid>d5e4261d-71f4-49d7-acc3-0b81b1f45862</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Creditline/creditline01</testCaseId>
+      <testCaseId>Test Cases/CreditLine/creditline01</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>2799faa4-5a6b-4995-8561-5830eba890b3</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Creditline/creditline02</testCaseId>
+      <testCaseId>Test Cases/CreditLine/creditline02</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>23d432d5-e502-4bec-ad8f-e0988a03fb81</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Creditline/creditline03</testCaseId>
+      <testCaseId>Test Cases/CreditLine/creditline03</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>7fd5d1e0-b5d2-439f-a9ed-8f07bd0d5ed9</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Creditline/creditline04</testCaseId>
+      <testCaseId>Test Cases/CreditLine/creditline04</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>59184d3d-bfaf-4a9b-bbca-af391647dc5a</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Creditline/creditline05</testCaseId>
+      <testCaseId>Test Cases/CreditLine/creditline05</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>fe906202-643d-4ad6-8862-45d9bf24e74f</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Creditline/creditline06</testCaseId>
+      <testCaseId>Test Cases/CreditLine/creditline06</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>37bd0583-0f8f-443e-9f7e-ab041bb9565c</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Creditline/creditline07</testCaseId>
+      <testCaseId>Test Cases/CreditLine/creditline07</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
